298db8ec87e910f0ab42378e69238fdc357a4558
Ｖａｓｉｌｉｙ <37266612+kurakin101@users.noreply.github.com>
2022-04-02 23:49:29 +0300

    12

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -567,9 +567,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "e632cd84e91b885897fe8e370c94f9e1",
-"/": "e632cd84e91b885897fe8e370c94f9e1",
-"main.dart.js": "4d1e9151a6fb484b7f5fb6e42c14218b",
+"index.html": "50f095231fd206c13b4a3747c939f773",
+"/": "50f095231fd206c13b4a3747c939f773",
+"main.dart.js": "556e66df70e4dc0f544011abd62f779d",
 "manifest.json": "66f3056d94a878a99aadbfe845af396e",
 "version.json": "b15ede7573cd8a8c47d8d1d14586050a"
 };
